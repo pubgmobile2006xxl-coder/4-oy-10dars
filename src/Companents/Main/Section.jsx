ac654195f7a6741ef55604b894b32c448eb7e1e6
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-21 12:09:00 +0500

    docifn

--- src/Companents/Main/Section.jsx
+++ src/Companents/Main/Section.jsx
@@ -1,1 +1,17 @@
 import './Section.css';
+import grop from '../../assets/Group 1.png'
+import { useTranslation } from 'react-i18next';
+
+import React from 'react'
+
+const Section = () => {
+  return (
+    <section>
+        <div className="container">
+            <h1>We are your MateDesigners</h1>
+        </div>
+    </section>
+  )
+}
+
+export default Section
