8a0ce1e8a8d4e833cc2486ef271899607cd34b78
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-21 12:30:45 +0500

    docifn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Companents/Header/Header'; 
 import Section from './Companents/Main/Section';
+import Sectioon from './Companents/Main/Sectioon';
 const App = () => {
   return (
     <div>
       <Header />
       <Section/>
+      <Sectioon/>
     </div>
   );
 };
---
+++ src/Companents/Main/Sectioon.jsx
@@ -0,0 +1,21 @@
+import grop from '../../assets/Rectangle 2.png'
+import './Sectioon.css';
+import { useTranslation } from 'react-i18next';
+
+import React from 'react'
+
+const Sectioon = () => {
+  return (
+    <sectioon>
+        <div className="container">
+            <div className="cards">
+                <div className="card">
+                    <img src={} alt="rasm" />
+                </div>
+            </div>
+        </div>
+    </sectioon>
+  )
+}
+
+export default Sectioon
